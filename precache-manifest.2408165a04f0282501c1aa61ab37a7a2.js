dabfe246bbe83e5a9cae1b808bc4bc4b4fb9167e
Somesh <someshkumarbhaskar@gmail.com>
2020-08-09 22:55:01 +0530

    Updates

---
+++ precache-manifest.2408165a04f0282501c1aa61ab37a7a2.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0276d988f243b48627d5a3c8bfa2e207",
+    "url": "/ChatBotReact/index.html"
+  },
+  {
+    "revision": "697a483cc9d4d5b26cdd",
+    "url": "/ChatBotReact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5312461a25d6a1784c8d",
+    "url": "/ChatBotReact/static/js/2.710e92cb.chunk.js"
+  },
+  {
+    "revision": "afcffdf4ebf1780e0970711996cdd7dd",
+    "url": "/ChatBotReact/static/js/2.710e92cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "697a483cc9d4d5b26cdd",
+    "url": "/ChatBotReact/static/js/main.4ab658d5.chunk.js"
+  },
+  {
+    "revision": "0184054c6dd4120d3bc6",
+    "url": "/ChatBotReact/static/js/runtime-main.244e448c.js"
+  },
+  {
+    "revision": "c630267b2b0520bca43688074556320b",
+    "url": "/ChatBotReact/static/media/airbnb.c630267b.svg"
+  },
+  {
+    "revision": "426fb3e81e37aa2edeaa2bd8b9775d95",
+    "url": "/ChatBotReact/static/media/akachi-luccini@2x.426fb3e8.jpg"
+  },
+  {
+    "revision": "18369218bccb35d3fe620be48f307a49",
+    "url": "/ChatBotReact/static/media/atlassian.18369218.svg"
+  },
+  {
+    "revision": "795930153f23de3ba0314aaf9cc1eabd",
+    "url": "/ChatBotReact/static/media/coinbase.79593015.svg"
+  },
+  {
+    "revision": "5e09ece6910218f68b747dbdb39ec4ac",
+    "url": "/ChatBotReact/static/media/dashboard-drawing.5e09ece6.svg"
+  },
+  {
+    "revision": "3baf9987979c71e3510cc69b6afd0502",
+    "url": "/ChatBotReact/static/media/dashboard-extended.3baf9987.svg"
+  },
+  {
+    "revision": "2b3e5af896b6f39acd4cbbe8ea29b13b",
+    "url": "/ChatBotReact/static/media/dribbble.2b3e5af8.svg"
+  },
+  {
+    "revision": "ab31ab119f963ad0cd4cf58c9640c1df",
+    "url": "/ChatBotReact/static/media/dropbox.ab31ab11.svg"
+  },
+  {
+    "revision": "8a245eedd1b8df9429f82a1f5d8bb76e",
+    "url": "/ChatBotReact/static/media/google-ad-manager.8a245eed.svg"
+  },
+  {
+    "revision": "c47aadb5b9721f00bf69df5f94fc1c78",
+    "url": "/ChatBotReact/static/media/google-drive.c47aadb5.svg"
+  },
+  {
+    "revision": "a1db1dc218e1226432aebdb39f267b0a",
+    "url": "/ChatBotReact/static/media/instagram.a1db1dc2.svg"
+  },
+  {
+    "revision": "be2cf876874f01272c32874ee3963245",
+    "url": "/ChatBotReact/static/media/jack-smith@2x.be2cf876.jpg"
+  },
+  {
+    "revision": "15c60bf3502cc66d839c766591f86092",
+    "url": "/ChatBotReact/static/media/logo-negative.15c60bf3.svg"
+  },
+  {
+    "revision": "9cf6d866f0afdf4997f3000277afb182",
+    "url": "/ChatBotReact/static/media/logo.9cf6d866.svg"
+  },
+  {
+    "revision": "f7497b3397c4c78dded8e58f67c3e0a9",
+    "url": "/ChatBotReact/static/media/mailchimp.f7497b33.svg"
+  },
+  {
+    "revision": "b3e8efc231a689daef7ec95058152079",
+    "url": "/ChatBotReact/static/media/map.b3e8efc2.png"
+  },
+  {
+    "revision": "40d2f5ac2b8e52c58eb0628961c058b3",
+    "url": "/ChatBotReact/static/media/mind-map.40d2f5ac.svg"
+  },
+  {
+    "revision": "67bdf544cfd1d33e01b0db6aad1ea8cd",
+    "url": "/ChatBotReact/static/media/netflix.67bdf544.svg"
+  },
+  {
+    "revision": "6c116a23b7f8907142a534793cce071b",
+    "url": "/ChatBotReact/static/media/pinterest.6c116a23.svg"
+  },
+  {
+    "revision": "4e9ec5619d1b5d0cdc84978fe795f788",
+    "url": "/ChatBotReact/static/media/place3.4e9ec561.jpg"
+  },
+  {
+    "revision": "d20e7064ed60c19f1e893807fd7b2de0",
+    "url": "/ChatBotReact/static/media/place3@2x.d20e7064.jpg"
+  },
+  {
+    "revision": "1de7ab9cc01afec8186f82bfec593704",
+    "url": "/ChatBotReact/static/media/slack.1de7ab9c.svg"
+  },
+  {
+    "revision": "b7a1f29bd69b747b88c1b80b5411bd79",
+    "url": "/ChatBotReact/static/media/veronica-adams@2x.b7a1f29b.jpg"
+  }
+]);
